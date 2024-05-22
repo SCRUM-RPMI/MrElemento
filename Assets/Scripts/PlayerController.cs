5bd1489af6696f0ddfd7d2e536446db899ae6691
softdevilan <softdevilan@proton.me>
2024-05-22 09:15:06 +0200

    script corrutina enemigo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = System.Random;
 
 public class PlayerController : MonoBehaviour
 {
